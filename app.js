3d189f38eafcc4bd8e41117b8700a8867da82306
sethimayank240-gif <sethimayank240@gmail.com>
2025-08-27 18:23:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn"); 
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let turnO = true; 
+let moves = 0; 
+
+const winPatterns = [
+  [0,1,2], [0,3,6], [0,4,8],
+  [1,4,7], [2,5,8], [2,4,6],
+  [3,4,5], [6,7,8],
+];
+
+const resetGame = () => {
+  turnO = true;
+  moves = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      box.classList.add("o");   // ✅ add O class
+    } else {
+      box.innerText = "X";
+      box.classList.add("x");   // ✅ add X class
+    }
+    box.disabled = true;
+    turnO = !turnO;
+    moves++;
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  boxes.forEach((box) => box.disabled = true);
+};
+
+const enableBoxes = () => {
+  boxes.forEach((box) => {
+    box.disabled = false;
+    box.innerText = "";
+    box.classList.remove("x", "o"); // ✅ remove colors when reset
+  });
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `🎉 Congratulations! Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const showDraw = () => {
+  msg.innerText = "😐 It's a Draw!";
+  msgContainer.classList.remove("hide");
+};
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let [a, b, c] = pattern;
+    if (
+      boxes[a].innerText !== "" &&
+      boxes[a].innerText === boxes[b].innerText &&
+      boxes[b].innerText === boxes[c].innerText
+    ) {
+      showWinner(boxes[a].innerText);
+      return;
+    }
+  }
+
+  if (moves === 9) {
+    showDraw();
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
